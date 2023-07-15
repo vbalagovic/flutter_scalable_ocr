9301340eaeeba6f28ad5a1bad445571e4d90c9e1
Ausbel <mr.rochahernandez@gmail.com>
2023-07-15 14:05:51 -0600

    remove commented code

--- lib/flutter_scalable_ocr.dart
+++ lib/flutter_scalable_ocr.dart
@@ -239,30 +239,12 @@ class ScalableOCRState extends State<ScalableOCR> {
         InputImageFormatValue.fromRawValue(image.format.raw);
     if (inputImageFormat == null) return;
 
-    // final planeData = image.planes.map(
-    //   (Plane plane) {
-    //     return InputImageMetadata(
-    //       size: imageSize,
-    //       rotation: imageRotation,
-    //       format: inputImageFormat,
-    //       bytesPerRow: plane.bytesPerRow,
-    //     );
-    //   },
-    // );
-
     final planeData = InputImageMetadata(
       size: imageSize,
       rotation: imageRotation,
       format: inputImageFormat,
       bytesPerRow: image.planes[0].bytesPerRow,
     );
-
-    // final inputImageData = InputImageData(
-    //   size: imageSize,
-    //   imageRotation: imageRotation,
-    //   inputImageFormat: inputImageFormat,
-    //   planeData: planeData,
-    // );
 
     final inputImage =
         // InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
